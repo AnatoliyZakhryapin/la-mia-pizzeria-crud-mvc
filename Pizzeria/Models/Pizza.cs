f161bc25cc34ca243ac8a1ecabe083aad30faeaf
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-15 14:53:01 +0200

    Add project files.

---
+++ Pizzeria/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace Pizzeria.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FotoUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public Pizza( string name, string description, string fotoUrl, decimal price)
+        {
+            Name = name;
+            Description = description;
+            FotoUrl = fotoUrl;
+            Price = price;
+        }
+    }
+}
