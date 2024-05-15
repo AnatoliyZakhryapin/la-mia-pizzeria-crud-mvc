81e2f5e59721e82278df5906a28d6429ff454ee5
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-15 23:20:05 +0200

    REFACTOR actione Index of PizzeriaController

--- Pizzeria/Controllers/PizzeriaController.cs
+++ Pizzeria/Controllers/PizzeriaController.cs
@@ -5,17 +5,31 @@ namespace Pizzeria.Controllers
 {
     public class PizzeriaController : Controller
     {
+        public PizzeriaDatabaseContext DatabaseContext = new PizzeriaDatabaseContext();
         public IActionResult Index()
         {
-            List<Pizza> pizze = new List<Pizza>();
 
-            pizze.Add(new Pizza("Margherita", "Pola di pomodoro, Fiordilatte, Origano, Basilico", "~/img/pizza_margherita.png", 7.99m));
-            pizze.Add(new Pizza("Prosciutto e mozzarella", "Pola di pomodoro, Fiordilatte, Prosciutto", "~/img/pizza_prosciutto.png", 9.99m));
-            pizze.Add(new Pizza("Divola", "Pola di pomodoro, Fiordilatte, Salame", "~/img/pizza_diavola.png", 10.99m));
-            pizze.Add(new Pizza("Quattro formaggi", "Mozzarella, Gorgonzola, Fontina, Parmigiano, Polpa di pomodoro", "~/img/pizza_quattro_formaggi.png", 11.99m));
-            pizze.Add(new Pizza("Napoletana", "Mozzarella, pomodoro, acciughe, origano, olio d’oliva Polpa di pomodoro", "~/img/pizza_napoletana.png", 10.99m));
+            if (DatabaseContext.Pizzas.Count() == 0)
+            {
+                List<Pizza> listaPizze = new List<Pizza>();
 
-            return View(pizze);
+                listaPizze.Add(new Pizza("Margherita", "Pola di pomodoro, Fiordilatte, Origano, Basilico", "~/img/pizza_margherita.png", 7.99m));
+                listaPizze.Add(new Pizza("Prosciutto e mozzarella", "Pola di pomodoro, Fiordilatte, Prosciutto", "~/img/pizza_prosciutto.png", 9.99m));
+                listaPizze.Add(new Pizza("Divola", "Pola di pomodoro, Fiordilatte, Salame", "~/img/pizza_diavola.png", 10.99m));
+                listaPizze.Add(new Pizza("Quattro formaggi", "Mozzarella, Gorgonzola, Fontina, Parmigiano, Polpa di pomodoro", "~/img/pizza_quattro_formaggi.png", 11.99m));
+                listaPizze.Add(new Pizza("Napoletana", "Mozzarella, pomodoro, acciughe, origano, olio d’oliva Polpa di pomodoro", "~/img/pizza_napoletana.png", 10.99m));
+
+                try
+                {
+                    DatabaseContext.AddRange(listaPizze);
+                    DatabaseContext.SaveChanges();
+                }
+                catch (Exception) { }
+            }
+
+            List<Pizza> listaPizzas = DatabaseContext.Pizzas.ToList();
+
+            return View(listaPizzas);
         }
 
         public IActionResult Show(Pizza pizza)
