bfb2afa2d52c6339b23064e060abb45cbbbd10a8
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-15 15:07:06 +0200

    ADD Controller PizzeriaController

---
+++ Pizzeria/Controllers/PizzeriaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pizzeria.Models;
+
+namespace Pizzeria.Controllers
+{
+    public class PizzeriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Pizza> pizze = new List<Pizza>();
+
+            pizze.Add(new Pizza("Margherita", "Pola di pomodoro, Fiordilatte, Origano, Basilico", "~/img/pizza_margherita.png", 7.99m));
+            pizze.Add(new Pizza("Prosciutto e mozzarella", "Pola di pomodoro, Fiordilatte, Prosciutto", "~/img/pizza_prosciutto.png", 9.99m));
+            pizze.Add(new Pizza("Divola", "Pola di pomodoro, Fiordilatte, Salame", "~/img/pizza_diavola.png", 10.99m));
+            pizze.Add(new Pizza("Quattro formaggi", "Mozzarella, Gorgonzola, Fontina, Parmigiano, Polpa di pomodoro", "~/img/pizza_quattro_formaggi.png", 11.99m));
+            pizze.Add(new Pizza("Napoletana", "Mozzarella, pomodoro, acciughe, origano, olio d’oliva Polpa di pomodoro", "~/img/pizza_napoletana.png", 10.99m));
+
+            return View(pizze);
+        }
+    }
+}
