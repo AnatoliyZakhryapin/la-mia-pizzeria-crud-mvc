60261c27e4eb67d670c14fb31dab04b516f1fce5
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-15 23:19:06 +0200

    ADD unique to Name of Pizza

--- Pizzeria/Models/Pizza.cs
+++ Pizzeria/Models/Pizza.cs
@@ -1,14 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Pizzeria.Models
 {
-
+    [Table("pizzas")]
+    [Index(nameof(Name), IsUnique = true)]
     public class Pizza
     { 
         public long PizzaId {  get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string FotoUrl { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
         public Pizza() { }
