57c929d4b26f9276664ad85176de3f642845a62b
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-16 14:21:18 +0200

    ADD Nullable to FotoUrl of Pizza, Create Migratione and Update DB

--- Pizzeria/Migrations/20240516121816_GetNullableFotoPizza.Designer.cs
+++ Pizzeria/Migrations/20240516121816_GetNullableFotoPizza.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pizzeria.Data;
 
@@ -10,9 +11,10 @@ using Pizzeria.Data;
 namespace Pizzeria.Migrations
 {
     [DbContext(typeof(PizzeriaDatabaseContext))]
-    partial class PizzeriaDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240516121816_GetNullableFotoPizza")]
+    partial class GetNullableFotoPizza
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Pizzeria/Models/Pizza.cs
+++ Pizzeria/Models/Pizza.cs
@@ -11,7 +11,7 @@ namespace Pizzeria.Models
         public long PizzaId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string FotoUrl { get; set; }
+        public string? FotoUrl { get; set; }
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
